6dab768786063198c2eb51fcb50289736223e153
Marcos Fábio <marcaum54@gmail.com>
2016-09-27 17:07:32 -0300

    enviando

--- coffee_rotation/views.py
+++ coffee_rotation/views.py
@@ -1,25 +1,35 @@
+import re
+from random import randint
+from datetime import datetime
 from django.shortcuts import render
+from django.contrib import messages
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse_lazy
-from datetime import datetime
 
 from django.contrib.auth.models import User
-from coffee_rotation.models import Cycle
-from coffee_rotation.models import Turn
-from django.contrib import messages
+from coffee_rotation.models.Cycle import Cycle
+from coffee_rotation.models.Turn import Turn
 
 
-def users_list(request):
+def list(request):
 
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (reverse_lazy('admin:login'), request.path))
 
     cycle = Cycle.objects.last()
 
-    usuarios = User.objects.all()
+    if cycle is None:
+        cycle = Cycle.objects.create(**{ 'name': 'Ciclo 01' })
 
-    return render(request, 'colaboradores/index.html', {
-        'usuarios': usuarios
+    turns = Turn.objects.filter(cycle=cycle)
+
+    calleds = Turn.objects.filter(date_removed__isnull=True).values_list('user', flat=True)
+
+    not_calleds = User.objects.exclude(pk__in=calleds)
+
+    return render(request, 'list.html', {
+        'turns': turns,
+        'not_calleds': not_calleds,
     })
 
 
@@ -28,13 +38,69 @@ def set_as_voluntary(request):
     try:
         user = request.user
         cycle = Cycle.objects.last()
-        voluntary_date = datetime.now()
+        date_voluntary = datetime.now()
 
-        Turn.objects.create(user=user, cycle=cycle, voluntary_date=voluntary_date)
+        Turn.objects.create(user=user, cycle=cycle, date_voluntary=date_voluntary)
 
         messages.success(request, 'Voluntariou-se com sucesso!')
 
     except ValueError as error:
+
+        messages.error(request, error)
+
+    return redirect(reverse_lazy('list'))
+
+
+def remove_turn(request, id):
+
+    try:
+        if request.method != 'POST':
+            raise ValueError('Requisição tem que ser POST')
+
+        turn = Turn.objects.get(id=id)
+
+        if turn.is_choosed() and request.user.id == turn.user_id:
+            raise ValueError('Você não pode remover um turno no qual você foi escolhido')
+
+        turn.date_removed = datetime.now()
+        turn.justification = request.POST['justification']
+        turn.removed_by_id = request.user.id
+
+        turn.save()
+
+        messages.success(request, 'Turno removido com sucesso!')
+
+    except ValueError as error:
+
+        messages.error(request, error)
+
+    return redirect(reverse_lazy('list'))
+
+
+def choose_randomly(request):
+
+    try:
+        if request.method != 'POST':
+            raise ValueError('Requisição tem que ser POST')
+
+        cycle = Cycle.objects.last()
+        turns = Turn.objects.filter(cycle=cycle)
+
+        calleds = Turn.objects.filter(date_removed__isnull=True).values_list('user', flat=True)
+        not_calleds = User.objects.exclude(pk__in=calleds)
+
+        if not not_calleds.count():
+
+            cycle = Cycle.objects.create(**{ 'name': re.sub('\d(?!\d)', lambda x: str(int(x.group(0)) + 1), cycle.name) })
+            turns = Turn.objects.filter(cycle=cycle)
+            not_calleds = User.objects.all()
+
+        choosed = randint(0, not_calleds.count() - 1)
+
+        messages.success(request, 'Turno removido com sucesso!')
+
+    except ValueError as error:
+
         messages.error(request, error)
 
     return redirect(reverse_lazy('list'))
--- settings.py
+++ settings.py
@@ -115,13 +115,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
